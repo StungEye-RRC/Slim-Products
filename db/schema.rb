6593a65ddf0d50ce58a8cdb31540d1c150aff32f
Kyle Geske <stungeye@gmail.com>
2017-03-15 21:03:14 +0000

    Added an image filename property to the products table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170315191358) do
+ActiveRecord::Schema.define(version: 20170315210044) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -47,8 +47,9 @@ ActiveRecord::Schema.define(version: 20170315191358) do
     t.string   "name"
     t.text     "description"
     t.decimal  "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.string   "image_filename"
   end
 
 end
