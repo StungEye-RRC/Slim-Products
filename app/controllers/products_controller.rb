1c6a63f221f5b8330f2f7dd539bdd1aca1457769
Kyle Geske <stungeye@gmail.com>
2017-03-15 20:19:30 +0000

    Added the products controller and edited the defaults.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,9 @@
+class ProductsController < ApplicationController
+  def index
+  end
+  # Auto load the view: app/views/products/index.html.slim
+
+  def show
+  end
+  # Auto load the view: app/views/products/show.html.slim
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  # Load the products#index on GET to /products
+  # Load the products#show on GET to /products/:id
+  resources 'products', only: [:index, :show]
+
+  # Load the products#index on GET to /
+  root to: 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
