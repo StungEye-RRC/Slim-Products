8ce56270dc8f13f301a7bfe9b23c62a1ae1cb7bc
Kyle Geske <stungeye@gmail.com>
2017-03-15 21:02:59 +0000

    Added the product partial and used it in the index and show view.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,11 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
   # Auto load the view: app/views/products/index.html.slim
 
   def show
+    @product = Product.find(params[:id])
   end
   # Auto load the view: app/views/products/show.html.slim
 end
